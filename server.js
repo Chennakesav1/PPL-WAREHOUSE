7f80ecf6aa98b6b2e0fe3b67fb6bef58347b2f8a
Chennakesav1 <chennakesavarao89@gmail.com>
2026-03-20 13:27:50 +0530

    product updated

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors({
 app.use(express.json());
 
 // Importing Models
-const { Product, Transaction, RawMaterial, PurchaseOrder, ProductionBatch } = require('./models');
+const { Product, Transaction, RawMaterial, PurchaseOrder, ProductionBatch, WorkOrder } = require('./models');
 
 
 
